13e5e8390c85e4ec5e159d3cab5bf38c15923dce
Benjamin Thom <jaminthom@gmail.com>
2020-03-12 21:43:01 -0700

    update RepeatCounterTest to include instances of RepeatCounter within tests

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -6,12 +6,46 @@ namespace Wordcounter.Tests
   [TestClass]
   public class RepeatCounterTests
   {
-    RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
-    
+
+    [TestMethod]
+    public void GetSentence_GetUserSentence_String()
+    {
+
+      //Arrange
+      RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
+      //Act
+      string sentence = RepeatCounter.GetSentence(testRepeatCounter.Sentence);
+      //Assert
+      Assert.AreEqual("the quick brown fox jumps over the lazy dog", sentence);
+    }
+
+    [TestMethod]
+    public void GetSentenceArray_GetUserSentenceAndConvertToArray_Arr()
+    {
+      //Arrange
+      RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
+      //Act
+      string[] sentenceArr = RepeatCounter.GetSentenceArray(testRepeatCounter.Sentence);
+      //Assert
+      Assert.AreEqual("the", sentenceArr[0]);
+    }
+
+    [TestMethod]
+    public void GetWord_GetUserWord_String()
+    {
+      //Arrange
+      RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
+      //Act
+      string word = RepeatCounter.GetWord(testRepeatCounter.Word);
+      //Assert
+      Assert.AreEqual("the", word);
+    }
+
     [TestMethod]
     public void GetRepeatWordFrequency_GetUserWordFrequencyFromUserSentence_Int()
     {
       //Arrange
+      RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
       string[] sentenceArr = { "the", "them", "they", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
       //Act
       int frequency = testRepeatCounter.GetRepeatWordFrequency();
@@ -20,3 +54,9 @@ namespace Wordcounter.Tests
     }
   }
 }
+
+    public RepeatCounterTests(string userSentece, string userWord)
+    {
+      userSentece = "the quick brown fox jumps over the lazy dog";
+      userWord = "the";
+    }
