2d4c5707d745ed8ea4f3caf60568753acad35f5a
Benjamin Thom <jaminthom@gmail.com>
2020-03-12 19:17:37 -0700

    test GetRepeatWordFrequency method and fail

--- WordCounter.Tests/ModelTests/RepeatCounterTests.cs
+++ WordCounter.Tests/ModelTests/RepeatCounterTests.cs
@@ -6,49 +6,50 @@ namespace Wordcounter.Tests
   [TestClass]
   public class RepeatCounterTests
   {
-    [TestMethod]
-    public void GetSentence_GetUserSentence_String()
-    {
-      //Arrange
-      string userSentence = "the quick brown fox jumps over the lazy dog";
-      //Act
-      string sentence = RepeatCounter.GetSentence(userSentence);
-      //Assert
-      Assert.AreEqual("the quick brown fox jumps over the lazy dog", sentence);
-    }
+    RepeatCounter testRepeatCounter = new RepeatCounter("the quick brown fox jumps over the lazy dog", "the");
 
-    [TestMethod]
-    public void GetSentenceArray_GetUserSentenceAndConvertToArray_Arr()
-    {
-      //Arrange
-      string userSentence = "the quick brown fox jumps over the lazy dog";
-      //Act
-      string[] sentenceArr = RepeatCounter.GetSentenceArray(userSentence);
-      //Assert
-      Assert.AreEqual("the", sentenceArr[0]);
-    }
+    // [TestMethod]
+    // public void GetSentence_GetUserSentence_String()
+    // {
+    //   //Arrange
+    //   string userSentence = "the quick brown fox jumps over the lazy dog";
+    //   //Act
+    //   string sentence = RepeatCounter.GetSentence(userSentence);
+    //   //Assert
+    //   Assert.AreEqual("the quick brown fox jumps over the lazy dog", sentence);
+    // }
 
-    [TestMethod]
-    public void GetWord_GetUserWord_String()
-    {
-      //Arrange
-      string userWord = "the";
-      //Act
-      string word = RepeatCounter.GetWord(userWord);
-      //Assert
-      Assert.AreEqual("the", word);
-    }
+    // [TestMethod]
+    // public void GetSentenceArray_GetUserSentenceAndConvertToArray_Arr()
+    // {
+    //   //Arrange
+    //   string userSentence = "the quick brown fox jumps over the lazy dog";
+    //   //Act
+    //   string[] sentenceArr = RepeatCounter.GetSentenceArray(userSentence);
+    //   //Assert
+    //   Assert.AreEqual("the", sentenceArr[0]);
+    // }
+
+    // [TestMethod]
+    // public void GetWord_GetUserWord_String()
+    // {
+    //   //Arrange
+    //   string userWord = "the";
+    //   //Act
+    //   string word = RepeatCounter.GetWord(userWord);
+    //   //Assert
+    //   Assert.AreEqual("the", word);
+    // }
 
     [TestMethod]
     public void GetRepeatWordFrequency_GetUserWordFrequencyFromUserSentence_Int()
     {
       //Arrange
       string[] sentenceArr = { "the", "them", "they", "quick", "brown", "fox", "jumps", "over", "the", "lazy", "dog" };
-      string word = "the";
       //Act
-      int frequency = RepeatCounter.GetRepeatWordFrequency(sentenceArr, word);
+      int frequency = testRepeatCounter.GetRepeatWordFrequency();
       //Assert
-      Assert.AreEqual(2, frequency);
+      Assert.AreEqual(3, frequency);
     }
   }
 }
--- WordCounter/Models/RepeatCounter.cs
+++ WordCounter/Models/RepeatCounter.cs
@@ -2,31 +2,42 @@ namespace WordCounter.Models
 {
   public class RepeatCounter
   {
+    public string Sentence { get; set; }
+    public string[] SentenceArr { get; set; }
+    public string Word {get; set; }
 
-    public static string GetSentence(string userSentence)
+    public RepeatCounter(string sentence, string word)
     {
-      string sentence = userSentence;
-      return sentence;
+      Sentence = sentence;
+      SentenceArr = Sentence.Split(' ');
+      Word = word;
     }
+
+
+    // public string GetSentence()
+    // {
+    //   string sentence = userSentence;
+    //   return sentence;
+    // }
     
-    public static string[] GetSentenceArray(string userSentence)
-    {
-      string[] sentenceArr = userSentence.Split(' ');
-      return sentenceArr;
-    }
+    // public string[] GetSentenceArray(string userSentence)
+    // {
+    //   string[] sentenceArr = userSentence.Split(' ');
+    //   return sentenceArr;
+    // }
 
-    public static string GetWord(string userWord)
-    {
-      string word = userWord;
-      return word;
-    }
+    // public string GetWord(string userWord)
+    // {
+    //   string word = userWord;
+    //   return word;
+    // }
 
-    public static int GetRepeatWordFrequency(string[] sentenceArr, string word)
+    public int GetRepeatWordFrequency()
     {
       int frequency = 0;
 
-      for (int i = 0; i < sentenceArr.Length; i++)
-      if (word.Equals(sentenceArr[i]))
+      for (int i = 0; i < SentenceArr.Length; i++)
+      if (Word.Equals(SentenceArr[i]))
       {
         frequency++;
       }
